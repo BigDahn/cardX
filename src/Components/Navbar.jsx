9b177636451d96c68432e58aed831b599f89f42e
BigDahn <harkinborholarholuwah@gmail.com.com>
2024-11-13 23:23:49 +0100

    final commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,19 @@
-
-
 const Navbar = () => {
   return (
     <div className="text-black font-Fira pb-20 pt-4">
       <div className="flex items-center h-[46px] justify-between align-element">
         <div>
           <h3 className="flex items-center">
-            Card<span className="font-bold text-3xl text-[#51cd97] italic">X</span>
+            Card
+            <span className="font-bold text-3xl text-[#51cd97] italic">X</span>
           </h3>
         </div>
         <ul className="hidden lg:flex gap-6">
           <li className="flex">
             Send & Receive
             <img
-              src="/public/images/arrow_drop_down_24dp_0A4C3E_FILL0_wght400_GRAD0_opsz24.svg"
-              alt=""
+              src="/images/arrow_drop_down_24dp_0A4C3E_FILL0_wght400_GRAD0_opsz24.svg"
+              alt="drop"
             />
           </li>
 
@@ -22,8 +21,8 @@ const Navbar = () => {
           <li className="flex">
             Offers
             <img
-              src="/public/images/arrow_drop_down_24dp_0A4C3E_FILL0_wght400_GRAD0_opsz24.svg"
-              alt=""
+              src="/images/arrow_drop_down_24dp_0A4C3E_FILL0_wght400_GRAD0_opsz24.svg"
+              alt="drop"
             />
           </li>
           <li className="flex">Contact Us</li>
@@ -36,7 +35,7 @@ const Navbar = () => {
           </button>
         </div>
         <div className="lg:hidden">
-          <img src="/public/images/menu.png" alt="" className="w-6" />
+          <img src="/images/menu.png" alt="hamburger" className="w-6" />
         </div>
       </div>
     </div>
